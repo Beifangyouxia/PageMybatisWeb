5b4829be5b133d6ee0c6e8dfd29682c0b2bfa943
sunchunchun <15350913572@163.com>
2019-05-21 08:25:55 +0800

    欧了

--- src/main/java/com/scc/test/TestOne.java
+++ src/main/java/com/scc/test/TestOne.java
@@ -1,8 +1,14 @@
 package com.scc.test;
 
+import org.junit.Test;
+
 /**
  * scc
  * 2019/5/21
  */
 public class TestOne {
+    @Test
+    public void testOne(){
+        System.out.println("我就测试一下而已！");
+    }
 }
